3fcb3cc8ed63f1272a88c7981b09f4b23feee492
ivan grachev <ivan@ivan.lol>
2024-03-31 18:09:27 +0300

    Initial Commit

---
+++ yoink-menu-bar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  yoink-menu-bar
+//
+//  Created by Ivan Grachev on 31/03/2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
