4e784d2bfb011c19b15433e2cd28ab8223b017b4
ivan grachev <ivan@ivan.lol>
2024-03-31 20:49:20 +0300

    tune size

--- yoink-menu-bar/YoinkViewController.swift
+++ yoink-menu-bar/YoinkViewController.swift
@@ -21,7 +21,6 @@ class YoinkViewController: NSViewController {
         super.viewDidLoad()
         let request = URLRequest(url: URL(string: "https://warpcast.com/~/channel/yoink")!)
         let webView = WKWebView(frame: .zero, configuration: webViewConfiguration)
-        webView.navigationDelegate = self
         webView.translatesAutoresizingMaskIntoConstraints = false
         containerView.addSubview(webView)
         NSLayoutConstraint.activate([
@@ -35,22 +34,6 @@ class YoinkViewController: NSViewController {
     
     @IBAction func quitButtonClicked(_ sender: Any) {
         NSApp.terminate(nil)
-    }
-    
-}
-
-extension YoinkViewController: WKNavigationDelegate {
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        let zoomLevel = 0.42
-        let widthPercentage = 100 / zoomLevel
-        let script = """
-            var scale = \(zoomLevel);
-            document.body.style.transform = 'scale(' + scale + ')';
-            document.body.style.transformOrigin = 'top left';
-            document.body.style.width = '\(widthPercentage)%';
-        """
-        webView.evaluateJavaScript(script, completionHandler: nil)
     }
     
 }
