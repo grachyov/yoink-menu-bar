76f6da91ffd420cb7658444f878b2d84763f0192
ivan grachev <ivan@ivan.lol>
2024-03-31 19:05:15 +0300

    create webview programmatically

--- yoink-menu-bar/YoinkViewController.swift
+++ yoink-menu-bar/YoinkViewController.swift
@@ -5,30 +5,32 @@ import WebKit
 
 class YoinkViewController: NSViewController {
     
-    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var containerView: NSView!
     @IBOutlet weak var quitButton: NSButton!
+    
+    private let sharedWebViewConfiguration: WKWebViewConfiguration = {
+        let config = WKWebViewConfiguration()
+        config.websiteDataStore = .default()
+        return config
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let request = URLRequest(url: URL(string: "https://google.com")!)
+        let request = URLRequest(url: URL(string: "https://warpcast.com/horsefacts.eth/0x68f8d903")!)
+        let webView = WKWebView(frame: .zero, configuration: sharedWebViewConfiguration)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(webView)
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            webView.leftAnchor.constraint(equalTo: containerView.leftAnchor),
+            webView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            webView.rightAnchor.constraint(equalTo: containerView.rightAnchor)
+        ])
         webView.load(request)
     }
     
-    @objc private func warnBeforeQuitting() {
-        let alert = NSAlert()
-        alert.messageText = "quit yoink?"
-        alert.alertStyle = .warning
-        alert.addButton(withTitle: "ok")
-        alert.addButton(withTitle: "cancel")
-        alert.buttons.last?.keyEquivalent = "\u{1b}"
-        let response = alert.runModal()
-        if response == .alertFirstButtonReturn {
-            NSApp.terminate(nil)
-        }
-    }
-    
     @IBAction func quitButtonClicked(_ sender: Any) {
-        warnBeforeQuitting()
+        NSApp.terminate(nil)
     }
     
 }
